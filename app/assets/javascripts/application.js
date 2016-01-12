810e04ff407c887ebf874d864436b9e52c659f28
Jamie Eula Evangelista <jevangelista@Jamies-MacBook-Pro.local>
2016-01-11 16:34:10 -0800

    got positioning of bartender speech bubble working. now working on actual bubble

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,38 +18,45 @@
 
 $(document).ready(function(){
 
- $('[data-toggle="tooltip"]').tooltip();
+/****** Start Emotions Page JS *******/ 
 
- $('[data-toggle="popover"]').popover();
+	$( ".bartender-img" ).click(function() {
+	  $('.right-bartender').show().text("Hello, my name is Tobi! I will be your personal bartender for today. Select one of the options below, and I'll serve just the drink for you!");
+	  console.log("You're click this!");
 
+	});
+
+/****** End Emotions Page JS *******/ 
+
+/****** Start Splash Page JS *******/ 
 //hover for left box
-$(".login-left").hover(function(){
+	$(".login-left").hover(function(){
 
-	  	$(this).addClass( "overlay" );
-	  	$(this).html('<div class="overlay-left">Come on in and join us!<br><br><a class="login-link" href="/sessions/new">Sign Up</a></div>');
-	      },function() {
-    	$(this).removeClass( "overlay" );
-    	$(this).html("New customers");
+		  	$(this).addClass( "overlay" );
+		  	$(this).html('<div class="overlay-left">Come on in and join us!<br><br><a class="login-link" href="/sessions/new">Sign Up</a></div>');
+		      },function() {
+	    	$(this).removeClass( "overlay" );
+	    	$(this).html("New customers");
 
 
-  }
-);
+	  }
+	);
 
-//hover for right box
-$(".login-right").hover(function(){
+	//hover for right box
+	$(".login-right").hover(function(){
 
-	  	$(this).addClass( "overlay" );
-	  	$(this).html('<div class="overlay-right">Step right in!<br><br><a class="login-link" href="/users/new">Login</a></div>');
-	      },function() {
-    	$(this).removeClass( "overlay" );
-    	$(this).html("Returning members");
-  }
-);
+		  	$(this).addClass( "overlay" );
+		  	$(this).html('<div class="overlay-right">Step right in!<br><br><a class="login-link" href="/users/new">Login</a></div>');
+		      },function() {
+	    	$(this).removeClass( "overlay" );
+	    	$(this).html("Returning members");
+	  }
+	);
+
+/****** End Splash Page JS *******/ 
 
 });
 
 
-// $splashBox.click(function(event) {
-//   	($this).addClass( "overlay" );
-// });
+
 
