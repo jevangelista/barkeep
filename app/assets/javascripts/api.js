d48093378411d4fa32a5f10407e9f59a7fe01f1f
Jamie Eula Evangelista <jevangelista@Jamies-MacBook-Pro.local>
2016-01-28 21:18:28 -0800

    got ajax call working with youtube api in jquery

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -1,41 +1,65 @@
-// Your use of the YouTube API must comply with the Terms of Service:
-// https://developers.google.com/youtube/terms
+$(document).ready(function(){
 
-// Helper function to display JavaScript value on HTML page.
-function showResponse(response) {
-    var responseString = JSON.stringify(response, '', 2);
-    document.getElementById('response').innerHTML += responseString;
-}
+var query = "manhattan"
 
-// Called automatically when JavaScript client library is loaded.
-function onClientLoad() {
-    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-}
+$.ajax({
+      url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=how+to+make+cocktail" + query + "&key=AIzaSyB1kuNQvVohmKYodg2I7pMs9ZY6-vT3QuQ", 
+      dataType: "jsonp",
+      success: function(data){
+               console.log(data);           
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+          alert (textStatus, + ' | ' + errorThrown);
+      }
+  });
 
-// Called automatically when YouTube API interface is loaded (see line 9).
-function onYouTubeApiLoad() {
-    // This API key is intended for use only in this lesson.
-    // See https://goo.gl/PdPA1 to get a key for your own applications.
-    gapi.client.setApiKey('AIzaSyB1kuNQvVohmKYodg2I7pMs9ZY6-vT3QuQ');
+});
 
-    search();
-}
 
-function search() {
-    // Use the JavaScript client library to create a search.list() API call.
-    var request = gapi.client.youtube.search.list({
-        part: 'snippet',
-        q: "Beatles"
+// $(document).ready(function(){
+
+//     console.log("preparing the request");
+
+
+// });
+
+// // Helper function to display JavaScript value on HTML page.
+// function showResponse(response) {
+//     var responseString = JSON.stringify(response, '', 2);
+//     document.getElementById('response').innerHTML += responseString;
+// }
+
+// // Called automatically when JavaScript client library is loaded.
+// function onClientLoad() {
+//     gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+// }
+
+// // Called automatically when YouTube API interface is loaded (see line 9).
+// function onYouTubeApiLoad() {
+//     // This API key is intended for use only in this lesson.
+//     // See https://goo.gl/PdPA1 to get a key for your own applications.
+//     gapi.client.setApiKey('AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE');
+
+//     search();
+// }
+
+// function search() {
+//     // Use the JavaScript client library to create a search.list() API call.
+//     var request = gapi.client.youtube.search.list({
+//         part: 'snippet',
+//         q: "Beatles"
  
 
-    });
+//     });
     
-    // Send the request to the API server,
-    // and invoke onSearchRepsonse() with the response.
-    request.execute(onSearchResponse);
-}
+//     // Send the request to the API server,
+//     // and invoke onSearchRepsonse() with the response.
+//     request.execute(onSearchResponse);
+//     console.log("just bout a cadillac")
+// }
 
-// Called automatically with the response of the YouTube API request.
-function onSearchResponse(response) {
-    showResponse(response);
-}
+// // Called automatically with the response of the YouTube API request.
+// function onSearchResponse(response) {
+//     showResponse(response);
+// }
+
